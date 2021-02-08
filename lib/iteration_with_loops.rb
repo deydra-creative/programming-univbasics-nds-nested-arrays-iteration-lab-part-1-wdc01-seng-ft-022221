866ee31799bc7c48c0db82cea33ce49ab09f5de9
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-08 03:25:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,9 @@ def find_even_values(src)
     [23, 41]
     ]
   
-  find_even_values(array_of_arrays)
+  if array_of_arrays.even?
+    p array_of_arrays
+  
   
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
